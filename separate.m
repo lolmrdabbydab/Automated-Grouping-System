69b672069164c6213d924161a5f8ef76a4ecfeb7
lolmrdabbydab <thaitimmy22@gmail.com>
2024-11-24 02:00:51 +0700

    Finished Separate Functionality

--- separate.m
+++ separate.m
@@ -1,4 +1,4 @@
-function [groupTable] = separate1(groupTable, nameList, separateMatrix)
+function [groupTable] = separate(groupTable, nameList, separateMatrix)
 
     % Iterate through each team
     for teamIndex = 1:size(separateMatrix, 1)
@@ -11,41 +11,68 @@ function [groupTable] = separate1(groupTable, nameList, separateMatrix)
         for individualIndex = 1:length(team)
         
             % Store the individual name & a list of their separate teammate
-            individual = team(individualIndex);
+            individual = team(individualIndex);  
             teammateList = team(~ismember(team, individual));
+
+            % If the individual is already added in the groupTable
+            if ismember(individual, groupTable)
+                
+                % Skip the individual (Don't add them again to groupTable)
+                continue;
+            end
 
             % Find a list of groups where the other member aren't in (can move to)
             teammateCountList = sum(ismember(groupTable, teammateList));
-            joinableGroupList = find(teammateCountList == 0);
+            groupWithoutTeammate = find(teammateCountList == 0);
+
+            % Store which of those group that still got a spot for individual to join
+            joinableGroupListIndex = find(sum(ismember(groupTable(:, groupWithoutTeammate), "0")) >= 1);
+            joinableGroupList = groupWithoutTeammate(joinableGroupListIndex);
             
             % If there's no group the individual can join
             if isempty(joinableGroupList)
-                
-                % Display warning message unable to separate the individual
-                warnMessage = sprintf(strcat("Unable to Separate ", individual, " from ", ...
-                    strjoin(teammateList(1:end-1), ", "), " and ", teammateList(end)));
-                warndlg(warnMessage, "Warning")
+
+                % If individual only have 1 teammate to separate
+                if length(teammateList) == 1
+                    % Create warning message unable to separate the individual from that teammate
+                    warnMessage = sprintf(strcat("\nUnable to Separate ", individual, " from ", teammateList(1), "\n"));
+
+                % If individual have >1 teammate to separate
+                else
+                    % Create warning message unable to separate the individual from those teammate
+                    warnMessage = sprintf(strcat("\nUnable to Separate ", individual, " from ", ...
+                        strjoin(teammateList(1:end-1), ", "), " and ", teammateList(end), "\n"));
+                end
+
+                % Display the warning message
+                warndlg(warnMessage, "Warning");
+            
+            % If there's group(s) the individual can join
             else
-                % Randomly choose a joinableGroupList
+                % Call joinRandomGroup() with parameters to choose a random position on groupTable to join
                 [addToRow, addToGroup] = joinRandomGroup(groupTable, joinableGroupList);
                 
-                % Add Individual to the group
+                % Add Individual to the position
                 groupTable(addToRow, addToGroup) = individual;
             end
         end
     end
 
-    % Randomly add the remaining individual from nameList to Table
-    
     % Find position of added individual
+    deletePos = ismember(nameList, groupTable);
     
     % Remove them from nameList
+    nameList(deletePos) = [];
     
     % Shuffle nameList
-    
+    nameList = nameList(randperm(length(nameList)));
+
     % Turn groupTable to a 1D array
-    
+    groupTableFlat = groupTable(:);
+
     % Add remaining names to "0"
-    
+    groupTableFlat(ismember(groupTableFlat, "0")) = nameList;
+
     % Reshape into matrix
+    groupTable = reshape(groupTableFlat, size(groupTable));
 end
