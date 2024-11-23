9f26cb1c373b2074823ca9f2f32ea34a17756ab7
lolmrdabbydab <thaitimmy22@gmail.com>
2024-11-24 01:53:11 +0700

    Incorporate Table to Simple Sorting Function

--- simple.m
+++ simple.m
@@ -1,19 +1,23 @@
-function [groupTable] = simple1()
+function [groupTable] = simple()
     
     % -= Variables & Input =-
     % Call on essentialGroupInput() to receive basic inputs for grouping
     [numGroup, numIndPerGroup, nameList] = essentialGroupInput();
     groupNameList = [];
 
-    % Randomize Name List
+    % Randomize Recieved Name List
     nameList = nameList(randperm(length(nameList)));
     
     % Reshape nameList into a matrix using correct dimensions
     groupTable = reshape(nameList, numIndPerGroup, numGroup);
 
     % Iterate through each group
-    
-        % Add Group Name To groupNameList
+    for column = 1:numGroup
+
+       % Add group Name to groupNameList
+       groupNameList = [groupNameList strcat("Group " + string(column))];
+    end
         
     % Change from matrix to table (better visually) & add group name using groupNameList
+    groupTable = array2table(groupTable, "VariableNames", groupNameList, "RowNames", {});
 end
